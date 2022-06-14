5567aeaeb8ac4453a92c1efb5874d65d080e99eb
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-06-14 10:33:25 -0300

    Adding config paths

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express"
 
+import TicketsRoutes from "@routes/tickets.routes"
+
 const app = express()
+
+app.use('/boletos', TicketsRoutes)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
 import app from "./app"
 
-
-app.get('/', (req, res) => {
-  return res.json({message: "Hello, World!"})
-})
-
 app.listen(8080, () => console.log("Server started!"))
